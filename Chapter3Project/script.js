36ae564ae89682c8d374002433949e30df4ad9bf
Ayahg2025 <ayahghazouli@gmail.com>
2025-09-24 21:29:57 -0500

    Add files via upload

---
+++ Chapter3Project/script.js
@@ -0,0 +1,29 @@
+// Created by Ayah Ghazouli on September 24, 2025
+
+// Full name
+let fullName = "Ayah Ghazouli";
+console.log("Full Name:", fullName);
+
+// Desired annual salary
+let desiredSalary = 55000;
+console.log("Desired Salary:", desiredSalary);
+
+// Veteran status
+let isVeteran = false;
+console.log("Veteran Status:", isVeteran);
+
+// Array of friends' names
+let friends = ["Rim", "Lilas", "Sara"];
+console.log("Friends:", friends);
+
+// Array of friends' desired salaries
+let friendsSalaries = [72000, 68000, 75000];
+console.log("Friends' Salaries:", friendsSalaries);
+
+// Literal object for another friend
+let anotherFriend = {
+  firstName: "Jana",
+  lastName: "Hassan",
+  desiredSalary: 88000
+};
+console.log("Another Friend:", anotherFriend);
